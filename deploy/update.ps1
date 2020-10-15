a142765938204fd687162ada46c3c0025bdb714b
Sauleil <sauleil@gmail.com>
2020-10-15 13:56:01 -0400

    updated scripts

--- deploy/update.ps1
+++ deploy/update.ps1
@@ -1,1 +1,11 @@
-sftp -b batchfile.txt sauleilc@sauleil.com
+If ($args[0] -Eq "all") {
+    Write-Host("Uploading local directories to server using ./all.txt");
+    sftp -b all.txt sauleilc@sauleil.com
+}
+If ($args[0] -Eq "site") {
+    Write-Host("Uploading local resources to server using ./site.txt");
+    sftp -b site.txt sauleilc@sauleil.com
+}
+else {
+    Write-Host("Usage: update.ps1 [all, site]");
+}
